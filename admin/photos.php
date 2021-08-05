c3b84ad53cd2b51a9583e79bbd55440379de14f4
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2021-08-06 01:45:28 +0200

    Style Make Structure More clear

--- admin/photos.php
+++ admin/photos.php
@@ -17,7 +17,7 @@
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">
-            Blank Page
+            Photos
             <small>Subheading</small>
         </h1>
         <ol class="breadcrumb">
